249ed957c30efb5009ff327f648db514fc651659
Santiago Altamiranda <santiagoaltamiranda1997@gmail.com>
2024-06-24 17:44:46 -0300

    Separo archivos y modificaciones

--- tp-react-1/src/App.jsx
+++ tp-react-1/src/App.jsx
@@ -1,13 +1,18 @@
-
 import './App.css'
 import Ejercicio1 from './componentes/Ejercicio1.jsx'
+import Ejercicio2 from './componentes/Ejercicio2.jsx'
 
-function App () {
-  return(
+function App() {
+  return (
     <div className="App">
-      <Ejercicio1 />
+      <div>
+        <Ejercicio1 />
+      </div>
+      <div>
+        <Ejercicio2 message="My friend" />
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
